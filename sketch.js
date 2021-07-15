c79c228b6d654944e9b08e23fff1e1aacd631da5
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-15 13:23:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Composite= Matter.Composite;
 const Constraint = Matter.Constraint;
 
 var engine, world;
