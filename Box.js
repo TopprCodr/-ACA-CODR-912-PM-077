31f52f32344b886d456d754d40b2d68427b396d8
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-15 13:24:06 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -9,7 +9,7 @@ class Box {
     this.width = width;
     this.height = height;
     
-    World.add(world, this.body);
+    Composite.add(world, this.body);
   }
   display(){
     var pos =this.body.position;
